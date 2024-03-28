095fa712e27f827d4a78dc368fec69d3fece2a96
PatriciaMkoji <keyshotrisha@gmail.com>
2024-03-28 22:28:03 +0300

    Updated app.js file which is the entry point to the app

--- src/app.js
+++ src/app.js
@@ -9,9 +9,29 @@ app.use(express.json());
 
 /* Routes */
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/blog-post', require('./routes/blogPostRoutes');
 
-/* Start the server */
+
+/* Database Connection */
+mongoose.connect('mongodb://localhost:27017/blogDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
+
+/* Error handling middleware */
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Somwthing went wrong!');
+});
+
+/* Start the Server */
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Server running on port ${PORT}');
+  console.log('Server is running on port ${PORT}');
 });
